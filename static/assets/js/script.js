a5c09d56ccbcd7bd66012211b6454e8b19aa8ca8
Victoria Rotaru <victoriarotaru1996@gmail.com>
2020-08-03 19:20:25 +0100

    Update script.js
    added endpoints

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,62 +1,108 @@
 // auth
 let tokenUrl = window.location.href.match(/\#(?:access_token)\=([\S\s]*?)\&/);
-let token = tokenUrl[1];
-// if (tokenUrl) {
-  
-//   localStorage.setItem('token', token);
 
-//   // get permissions from token
-//   try {
-//     let permissions;
-//     permissions = JSON.parse(atob(token.split('.')[1])).permissions;
-//     localStorage.setItem('permissions', permissions);
-//     if (!localStorage.getItem('permissions')) {
-//       permissions = ['delete:actor','delete:movie','get:actor','get:actor-detail','get:movie','patch:actor','patch:movie','post:actor','post:movie']; // default permissions
-//       localStorage.setItem('permissions', permissions);
-//     }
-//   } catch (e) {
-//     iziToast.error({
-//       title: "Error",
-//       message: e,
-//     });
-//   }
-// }else{
-//   document.getElementById('LogOutbutton').remove();
-// }
-
-
-
-
-
-
-// function getTokenResponse(){
-// var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "http://127.0.0.1:5000/movies",
-//   "method": "GET",
-//   "headers": {
-//     'content-type': 'application/json',
-//     "authorization": "Bearer "+token
-//   }
-// }
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-// }
-
-$.ajax({
-  url: '/movies',
-  type: 'GET',
-  contentType: 'application/json',
-  headers: {
-     'Authorization': 'Bearer '+token
-  },
-  success: function (result) {
-      console.log(result)
-  },
-  error: function (error) {
-    console.log(error)
+if (tokenUrl) {
+  localStorage.setItem('token', tokenUrl[1]);
+  // get permissions from token
+  let permissions;
+  permissions = JSON.parse(atob(tokenUrl[1].split('.')[1])).permissions;
+  if(permissions.includes("get:movie") && permissions.includes("get:actor-detail")){
+    getMovies();
+    getActors();
+    document.getElementById('LogInbutton').remove();
   }
-});
+  if(permissions.includes("post:movie") && permissions.includes("post:actor")){
+    //addMovie();
+    //addActors();
+  }
+}else{
+  document.getElementById('LogOutbutton').remove();
+}
+
+function getMovies(){
+  $.ajax({
+    url: '/movies',
+    type: 'GET',
+    contentType: 'application/json',
+    headers: {
+       'Authorization': 'Bearer '+tokenUrl[1]
+    },
+    success: function (result) {
+        console.log(result)
+    },
+    error: function (error) {
+      console.log(error)
+    }
+  });
+  
+}
+
+function getActors(){
+  $.ajax({
+    url: '/actors-detail',
+    type: 'GET',
+    contentType: 'application/json',
+    headers: {
+       'Authorization': 'Bearer '+tokenUrl[1]
+    },
+    success: function (result) {
+        console.log(result)
+    },
+    error: function (error) {
+      console.log(error)
+    }
+  });
+}
+
+function logOutSession(){
+  localStorage.clear()
+  window.location.href = 'https://vrdev.eu.auth0.com/v2/logout'
+  window.location.replace('/')
+}
+
+function addMovie(){
+  let dataSend = {
+    "title": "Vixic Test",
+    "release_date": "03/08/2020"
+  }
+  $.ajax({
+    url: '/movies',
+    type: 'POST',
+    contentType: 'application/json',
+    headers: {
+       'Authorization': 'Bearer '+tokenUrl[1]
+    },
+    dataType: 'json',
+    data: JSON.stringify(dataSend),
+    success: function (result) {
+        console.log(result)
+    },
+    error: function (error) {
+      console.log(error)
+    }
+  });
+}
+
+function addActors(){
+  let dataSend = {
+    "name": "Jhon Blade",
+    "age": "45",
+   "gender": "M" 
+  }
+  $.ajax({
+    url: '/actors',
+    type: 'POST',
+    contentType: 'application/json',
+    headers: {
+       'Authorization': 'Bearer '+tokenUrl[1]
+    },
+    dataType: 'json',
+    data: JSON.stringify(dataSend),
+    success: function (result) {
+        console.log(result)
+    },
+    error: function (error) {
+      console.log(error)
+    }
+  });
+}
