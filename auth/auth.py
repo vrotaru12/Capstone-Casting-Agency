87fef59dd6221e6110b754d67c4441433a39b634
Victoria Rotaru <victoriarotaru1996@gmail.com>
2020-08-03 12:21:58 +0100

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'vrdev.eu.auth0.com'
+AUTH0_DOMAIN = 'DOMAIN.eu.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'MovieCastApp'
+API_AUDIENCE = 'API_AUDIENCE'
 
 ## AuthError Exception
 '''
