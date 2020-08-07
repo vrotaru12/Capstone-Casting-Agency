6c126e8e8301965d2f9bec5b9da4aa1e1dbf73a0
Victoria Rotaru <victoriarotaru1996@gmail.com>
2020-08-07 10:54:52 +0100

    main funcionality
    changed functionality of app

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -6,17 +6,37 @@ if (tokenUrl) {
   // get permissions from token
   let permissions;
   permissions = JSON.parse(atob(tokenUrl[1].split('.')[1])).permissions;
+  profilechange(permissions);
   if(permissions.includes("get:movie") && permissions.includes("get:actor-detail")){
-    getMovies();
-    getActors();
     document.getElementById('LogInbutton').remove();
   }
   if(permissions.includes("post:movie") && permissions.includes("post:actor")){
-    //addMovie();
-    //addActors();
+    // addMovie();
+    // addActors();
   }
 }else{
   document.getElementById('LogOutbutton').remove();
+  document.getElementById('getActors').remove();
+  document.getElementById('getMovies').remove();
+}
+
+
+function profilechange(permission){
+  let node = document.createElement("LI"),textnode = document.createTextNode("Casting Director");
+  if(permission.includes("patch:movie") && permission.includes("patch:actor")){
+    document.getElementById('aboutUsbutton').remove();
+    document.getElementById('contactButton').remove();
+    document.getElementById("profileImage").src = "static/images/pic01-.jpg";
+    
+    node.appendChild(textnode); 
+    document.getElementById('nav').children[0].insertBefore(node,document.getElementById('nav').children[0].children[3]);
+    document.getElementById('nav').children[0].children[3].style.fontFamily = 'Comic Sans MS';
+    document.getElementById('nav').children[0].children[3].style.fontStyle = 'italic';
+
+  }else if(permission.includes("post:movie") && permission.includes("post:actor") 
+  && !permission.includes("patch:actor") && !permission.includes("patch:movie")){
+    document.getElementById("profileImage").src = "static/images/pic01--.jpg";
+  }
 }
 
 function getMovies(){
@@ -52,6 +72,7 @@ function getActors(){
       console.log(error)
     }
   });
+  
 }
 
 function logOutSession(){
